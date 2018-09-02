192e80b7ad198069397da67dfc9dd25ddd521e5a
RCleary <RCleary@gmcps.com>
2018-09-01 21:18:37 -0500

    working plz

--- npl_app/settings.py
+++ npl_app/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+# DB:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
