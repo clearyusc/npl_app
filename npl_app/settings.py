34933f74dd6917a5c77e5d681469973a4da18fd8
RCleary <RCleary@gmcps.com>
2018-09-01 21:20:01 -0500

    plz

--- npl_app/settings.py
+++ npl_app/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
     }
 }
 # DB:
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 
 # Password validation
