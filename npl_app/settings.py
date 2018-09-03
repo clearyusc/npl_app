6803fc00498738a9ed739f799b9c9e09946c7f62
RCleary <RCleary@gmcps.com>
2018-09-03 17:08:02 -0500

    trying..

--- npl_app/settings.py
+++ npl_app/settings.py
@@ -17,7 +17,9 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'npl', 'static')
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'npl', 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
